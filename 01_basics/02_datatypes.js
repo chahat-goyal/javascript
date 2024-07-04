201d8f6efc018fd2e47ebd1019a826887e9837b6
chahat-goyal <142155179+chahat-goyal@users.noreply.github.com>
2024-07-04 09:39:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+ "use strict";    //treat all JS code as newer version 
+
+//  alert(3+3)  //we are using nodejs, not browser
+
+console.log(3+3)
+
+
+let name = "chahat"
+let age = 18
+let isLogin = false
+
+// prmitive datatypes
+
+// range of number:  2 to power 53 
+// bigint
+// string= ""
+// boolean = true/false
+// null: standalone value(empty)
+// undefined: variable declare kra bt value ni di
+// symbol: uniqueness
+
+
+// object
+
+console.log(typeof "chahat")
+console.log(typeof age)
+console.log(typeof null)    //object
+console.log(typeof undefined)   //undefined
